a715c6ea28453d0906c775c1e71bb2ce46498a59
Artyom <artvirsh@mail.ru>
2018-09-13 12:09:38 +0300

    Edit decision

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
 module.exports = function solveSudoku(matrix) {
     for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix.length; j++){
-          if(matrix[i][j] === 0){      
-  
-          for(let x = 1; x < 10; x++){
-              if(elementVaild(x, i, j, matrix)){
-                matrix[i][j] = x;
-              let arrVailid = solveSudoku(matrix);
-              if(arrVailid !== false) return arrVailid;
+        for(let j = 0; j < matrix.length; j++) {
+            if(matrix[i][j] === 0) {        
+                for(let x = 1; x < 10; x++){
+                    if(elementVaild(x, i, j, matrix)){
+                        matrix[i][j] = x;
+                        let arrVailid = solveSudoku(matrix);
+                        if(arrVailid) return arrVailid;
+                    }
+                }
+                matrix[i][j] = 0;
+                    
+                return false;
             }
-          }
-          matrix[i][j] = 0;
-            
-          return false;
         }
-      }
     }
     return matrix;
 }
 
-function elementVaild(num, i, j, matrix)
-{
-for (let y = 0; y < matrix.length; y++){
-    if (matrix[y][j] === num) return false;
-    if (matrix[i][y] === num) return false;
-}
-      
-let iM = Math.floor(i / 3) * 3, 
-    jM = Math.floor(j / 3) * 3;
-          
-for (let a = iM; a < iM + 3; a++){
-    for (let b = jM; b < jM + 3; b++){
-        if (matrix[a][b] === num) return false;
+function elementVaild(num, i, j, matrix) {
+    let iM = Math.floor(i / 3) * 3, 
+        jM = Math.floor(j / 3) * 3;
+        
+    for (let y = 0; y < matrix.length; y++){
+        if (matrix[y][j] === num || matrix[i][y] === num) return false;
     }
-}
+            
+    for (let a = iM; a < iM + 3; a++) {
+        for (let b = jM; b < jM + 3; b++){
+            if (matrix[a][b] === num) return false;
+        }
+    }
       
     return true;
 }
